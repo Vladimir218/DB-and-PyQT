8f643b7a1ed789a2703b74175b7385d586dd118f
Vladimir218 <kvv23@tut.by>
2022-09-11 16:37:23 +0300

    lesson2. Задание3 добавлена проверка портов (целое число и больше 0)

--- progect_messager/port_descriptor.py
+++ progect_messager/port_descriptor.py
@@ -7,9 +7,9 @@ logger = logging.getLogger('server_port_descriptor')
 
 class Check_port:
     def __set__(self,instance,port):
-        if not 1023<port<65536:
+        if (not 1023<port<65536) or (port>=0) or (port%1 == 0):
             logger.critical(f'Порт:{port} задан не корректно. '
-                              f'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+                              f'В качастве порта может быть указано только целое число в диапазоне от 1024 до 65535.')
             exit(1)
         instance.__dict__[self.my_attr] = port
 
